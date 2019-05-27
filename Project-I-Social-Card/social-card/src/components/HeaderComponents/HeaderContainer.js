2d5dbaf7c4e09ba5e51f4ed08273cd694d460af2
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-27 15:54:18 +0100

    created HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 const HeaderContainer = () => (
   <div>
     <ImageThumbnail 
       src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png' 
+      />
+
+    <HeaderTitle 
+      text='Lambda School' handle='LambdaSchool'
       />
   </div>
 );
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
+
+const timestamp = moment().format('DD MMM')
+
+const HeaderTitle = ({ text, handle }) => (
+  <div className='header-title'>
+    <h4>{text}</h4>
+    <p>{handle}</p>
+    <p>{timestamp}</p>
+  </div>
+);
+
+export default HeaderTitle;
