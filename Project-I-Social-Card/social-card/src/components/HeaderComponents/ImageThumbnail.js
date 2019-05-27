92728883607288ec1dfdeb9046a1ee4447a38d7c
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-27 15:34:45 +0100

    created ImageThumbnail component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = ({ src, alt }) => (
+  <div className='image-thumbnail'>
+    <img
+      src={src}
+      alt={alt}
+    />
+  </div>
+);
+
+export default ImageThumbnail;
