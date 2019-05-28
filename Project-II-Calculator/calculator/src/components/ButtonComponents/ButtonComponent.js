bc6ac5e5d38a9e9278ecf104c2fc13f562a9ed88
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-28 12:51:51 +0100

    Created the structure for the container component

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    Hello World The Button components will be here in a minute.
+  </div>
+);
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Display.css';
+
+export default () => (
+  <div>
+    Hello World The Display components will be here in a minute.
+  </div>
+);
