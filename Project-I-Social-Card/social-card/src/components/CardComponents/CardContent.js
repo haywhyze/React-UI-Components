97aba388e30992d5874f3042b399bc399a8c0e6b
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-28 09:28:55 +0100

    created CardContent with appropriate styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import './Card.css';
 
+const CardContent = ({ heading, text, link }) => (
+  <div className='card-content'>
+    <h4>{heading}</h4>
+    <p>{text}</p>
+    <p className='link'>{link}</p>
+  </div>
+);
+
+export default CardContent;
