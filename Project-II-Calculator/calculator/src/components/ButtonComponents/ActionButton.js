666a8a9e47386bdc4d3c254c3bc12f36884ade4a
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-28 15:17:32 +0100

    Add actionButtonComponent and styled accordingly

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Button.css';
+
+export default () => (
+  <table className='action'>
+    <tbody>
+      <tr>
+        <td id='divide'>÷</td>
+      </tr>
+      <tr>
+        <td>x</td>        
+      </tr>
+      <tr>
+        <td>–</td>
+      </tr>
+      <tr>
+        <td>+</td>
+      </tr>
+      <tr>
+        <td>=</td>
+      </tr>
+    </tbody>
+  </table>
+);
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonComponent.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Number from './NumberButton';
+import Action from './ActionButton';
 import './Button.css';
 
 export default () => (
-  <>
+  <div className='button-container'>
     <Number />
-  </>
+    <Action />
+  </div>
 );
