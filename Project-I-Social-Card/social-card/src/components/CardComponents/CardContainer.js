97a389e673bbfd8c68714ba42402e0f5d7f6f953
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-28 06:59:16 +0100

    Created CardBanner and styled it

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CardBanner from './CardBanner';
 import './Card.css';
 
 const CardContainer = () => (
-  <div>
-    {/* Insert Card Componets in here */}
+  <div className='card-container'>
+    <CardBanner bgColor='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png
+'/>
   </div>
 );
 
