ae63766a2d0295b16673bbf642306aadb91c7f49
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-28 13:28:37 +0100

    styled calculator display

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Display.css';
 
 export default () => (
-  <div>
-    Hello World The Display components will be here in a minute.
+  <div class='calculator-display'>
+    <p className='display-digit'>0</p>
   </div>
 );
