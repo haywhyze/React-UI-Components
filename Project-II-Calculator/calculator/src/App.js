3a958be6781463e493114424a975978261e354da
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-29 03:08:01 +0100

    add calculator symbols data and looped through it to render buttons based on the data

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,85 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
+const calculatorData = [
+  {
+    text: 'clear',
+    buttonStyles: 'actionButton'
+  },
+  {
+    text: '÷',
+    buttonStyles: 'symbolButton'
+  },
+  {
+    text: '7',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '8',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '9',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '×',
+    buttonStyles: 'symbolButton'
+  },
+  {
+    text: '4',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '5',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '6',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '–',
+    buttonStyles: 'symbolButton'
+  },
+  {
+    text: '3',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '2',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '1',
+    buttonStyles: 'numberButton'
+  },
+  {
+    text: '+',
+    buttonStyles: 'symbolButton'
+  },
+  {
+    text: '0',
+    buttonStyles: 'actionButton'
+  },
+  {
+    text: '=',
+    buttonStyles: 'symbolButton'
+  },
+]
 
 const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      {
+        calculatorData.map(data => {
+          return (
+            <NumberButton text={data.text} buttonStyles={data.buttonStyles}/>
+          );
+        })
+      }
     </div>
   );
 };
