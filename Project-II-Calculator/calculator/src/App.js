eb348d253c0429d86ca0d9c81ffb4f97fdabe1ff
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-29 04:16:11 +0100

    add a button container div to house the calculator buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,66 +4,82 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 
 const calculatorData = [
   {
+    id: 0,
     text: 'clear',
     buttonStyles: 'actionButton'
   },
   {
+    id: 1,
     text: '÷',
     buttonStyles: 'symbolButton'
   },
   {
+    id: 2,
     text: '7',
     buttonStyles: 'numberButton'
   },
   {
+    id: 3,
     text: '8',
     buttonStyles: 'numberButton'
   },
   {
+    id: 4,
     text: '9',
     buttonStyles: 'numberButton'
   },
   {
+    id: 5,
     text: '×',
     buttonStyles: 'symbolButton'
   },
   {
+    id: 6,
     text: '4',
     buttonStyles: 'numberButton'
   },
   {
+    id: 7,
     text: '5',
     buttonStyles: 'numberButton'
   },
   {
+    id: 8,
     text: '6',
     buttonStyles: 'numberButton'
   },
   {
+    id: 9,
     text: '–',
     buttonStyles: 'symbolButton'
   },
   {
+    id: 10,
     text: '3',
     buttonStyles: 'numberButton'
   },
   {
+    id: 11,
     text: '2',
     buttonStyles: 'numberButton'
   },
   {
+    id: 12,
     text: '1',
     buttonStyles: 'numberButton'
   },
   {
+    id: 13,
     text: '+',
     buttonStyles: 'symbolButton'
   },
   {
+    id: 14,
     text: '0',
     buttonStyles: 'actionButton'
   },
   {
+    id: 15,
     text: '=',
     buttonStyles: 'symbolButton'
   },
@@ -73,13 +89,15 @@ const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
-      {
-        calculatorData.map(data => {
-          return (
-            <NumberButton text={data.text} buttonStyles={data.buttonStyles}/>
-          );
-        })
-      }
+      <div className='buttonContainer'>
+        {
+          calculatorData.map(data => {
+            return data.buttonStyles !== 'actionButton' ? 
+              (<NumberButton key={data.id} text={data.text} buttonStyles={data.buttonStyles}/>) :
+              (null);
+          })
+        }
+      </div>
     </div>
   );
 };
